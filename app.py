5a28c0273b88ff049e0fb4db2c8719dec844824d
Andromed628 <s6591797@gmail.com>
2026-09-14 00:58:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -215,9 +215,11 @@ with gr.Blocks(theme=gr.themes.Soft(), title="TechQuest 2026 - Lunar Ice AI") as
                 inputs=[slope_input, cpr_land, ice_prob_land],
                 outputs=landing_output,
             )
-
 import os
 
+# Get port assigned by Render, default to 7860 if local
 port = int(os.environ.get("PORT", 7860))
+
 demo.launch(server_name="0.0.0.0", server_port=port)
+
 
